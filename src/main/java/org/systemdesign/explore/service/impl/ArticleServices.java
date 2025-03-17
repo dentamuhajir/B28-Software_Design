052bef92305c6c6ff2d18e416d7973ae3fd8cd09
Denta Muhajir <dentamuhajir@gmail.com>
2025-03-17 22:01:16 +0700

    Generic Type <T> and implementation to interface

---
+++ src/main/java/org/systemdesign/explore/service/impl/ArticleServices.java
@@ -0,0 +1,22 @@
+package org.systemdesign.explore.service.impl;
+
+import org.systemdesign.explore.service.Article;
+import org.systemdesign.explore.service.GenericServices;
+
+public class ArticleServices implements GenericServices<Article, Long> {
+    @Override
+    public void create(Article entity) {
+
+    }
+
+    @Override
+    public void read(Article entity) {
+
+    }
+
+    @Override
+    public void update(Article entity) {
+
+    }
+
+}
